82f80f54413eec980ca378db03fc7bae9120622a
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-06 19:26:59 +0530

    excluding embedded database from each serivce

--- microservices/product-service/src/test/java/com/luv4code/service/product/PersistenceTests.java
+++ microservices/product-service/src/test/java/com/luv4code/service/product/PersistenceTests.java
@@ -1,4 +1,8 @@
 package com.luv4code.service.product;
 
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 public class PersistenceTests extends MongoDbTestBase {
 }
