9a6eafef86cb91da7dbde8736652e6578b296ffd
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-08 09:13:06 +0530

    updating reactive test methods to the product and recommendation servivces

---
+++ util/src/test/java/com/luv4code/util/http/ReactorTests.java
@@ -0,0 +1,23 @@
+package com.luv4code.util.http;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReactorTests {
+    @Test
+    void testFlux() {
+        List<Integer> list = new ArrayList<>();
+
+        Flux.just(1, 2, 3, 4)
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .log()
+                .subscribe(n -> list.add(n));
+        assertThat(list).containsExactly(4, 8);
+    }
+}
