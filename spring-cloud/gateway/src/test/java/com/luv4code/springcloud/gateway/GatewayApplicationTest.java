6ac34ca131ebf6458cbb98021e42d12da77e0a8a
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-10 09:14:17 +0530

    adding spring cloud gateway service and adding routing configuration for all microservices

---
+++ spring-cloud/gateway/src/test/java/com/luv4code/springcloud/gateway/GatewayApplicationTest.java
@@ -0,0 +1,16 @@
+package com.luv4code.springcloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"eureka.client.enabled=false"})
+class GatewayApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
