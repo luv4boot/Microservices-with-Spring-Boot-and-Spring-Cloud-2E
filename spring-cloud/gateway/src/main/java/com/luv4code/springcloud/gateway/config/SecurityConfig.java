0bbb4b1395281a414035371404504bb0fa583c9c
luv4code-lc <madhav.p.akv@gmail.com>
2022-03-11 13:37:36 +0530

    updating maven dependencny versions

---
+++ spring-cloud/gateway/src/main/java/com/luv4code/springcloud/gateway/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.luv4code.springcloud.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/headerrouting/**").permitAll()
+                .pathMatchers("/actuator/**").permitAll()
+                .pathMatchers("/eureka/**").permitAll()
+                .pathMatchers("/oauth2/**").permitAll()
+                .pathMatchers("/login/**").permitAll()
+                .pathMatchers("/error/**").permitAll()
+                .pathMatchers("/openapi/**").permitAll()
+                .pathMatchers("/webjars/**").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+}
